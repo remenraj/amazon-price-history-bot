0124cd78afe1c3b8d78be03655d31dca10eca6fc
remenraj <remenraj@gmail.com>
2022-08-24 02:44:36 +0530

    Create basic bot

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
-import re
-from telegram import LabeledPrice, ParseMode
-from telegram.ext import PreCheckoutQueryHandler
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.filters import Filters
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.updater import Updater
-from telegram.update import Update
+import re, os, logging
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-if __name__ == "__main__":
+
+# setting up logging module, so you will know when (and why) things don't work as expected
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+      
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
     
-    url = "https://www.amazon.in/gp/product/B073YSQ927/ref=ewc_pr_img_1?smid=ARENK22K2PQRH&psc=1"
-    asin_id = re.findall('/\w{10}/', url)[0].strip("/")
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+        
+url = "https://www.amazon.in/gp/product/B073YSQ927/ref=ewc_pr_img_1?smid=ARENK22K2PQRH&psc=1"
+asin_id = re.findall('/\w{10}/', url)[0].strip("/")    
+
+def main() -> None:
+    """Start the bot."""
+    BOT_TOKEN = os.getenv("BOT_AMAZON_PRICE_HISTORY_TOKEN")
     
-    
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(BOT_TOKEN)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
     
